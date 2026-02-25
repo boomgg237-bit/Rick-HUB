1813f32de1cb4134f579fe65556331a1470c93be
boomgg237-bit <boomgg237@gmail.com>
2026-02-25 22:28:19 +0700

    Update LoaderRickHUB.lua

--- LoaderRickHUB.lua
+++ LoaderRickHUB.lua
@@ -1,1 +1,25 @@
-
+local scriptList = {
+    [155615604] = "https://raw.githubusercontent.com/Loader-Sp/RickHUBLoader/refs/heads/main/Prisonloader.lua", -- Prison Life
+  
+}
+local function Notify(title, content)
+    pcall(function()
+        game:GetService("StarterGui"):SetCore("SendNotification", {
+            Title = title,
+            Text = content,
+            Icon = "rbxassetid://108958018844079",
+            Duration = 5
+        })
+    end)
+end
+local currentPlaceId = game.PlaceId
+local targetScript = scriptList[currentPlaceId]
+if targetScript then
+    Notify("Rick HUB", "Loader success")
+    task.wait(0.5)
+    pcall(function()
+        loadstring(game:HttpGet(targetScript))()
+    end)
+else
+    Notify("Rick HUB", "MapNotSuppot")
+end

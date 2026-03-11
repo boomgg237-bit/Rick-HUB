b0409bbc46ab26c72e3699fcfe35860c316c68c1
boomgg237-bit <boomgg237@gmail.com>
2026-03-11 23:17:23 +0700

    Update LoaderRickHUB.lua

--- LoaderRickHUB.lua
+++ LoaderRickHUB.lua
@@ -2,8 +2,14 @@ local scriptList = {
     [155615604] = "https://raw.githubusercontent.com/Loader-Sp/RickHUBLoader/refs/heads/main/Prisonloader.lua", -- Prison Life
     [127794225497302] = "https://raw.githubusercontent.com/Loader-Sp/RickHUBLoader/refs/heads/main/Abyss.lua", -- Abyss
     [97556409405464] = "https://raw.githubusercontent.com/Loader-Sp/RickHUBLoader/refs/heads/main/Block%20spin.lua", -- Blockspinเซิฟ1
-    [104715542330896] = "https://raw.githubusercontent.com/Loader-Sp/RickHUBLoader/refs/heads/main/Block%20spin.lua",-- Blockspinเซิฟ2
+    [104715542330896] = "https://raw.githubusercontent.com/Loader-Sp/RickHUBLoader/refs/heads/main/Block%20spin.lua", -- Blockspinเซิฟ2 
+    [537413528] = {
+        hub = "RICKHUB",
+        file = "Build a Boat for Treasure",
+        url = "https://dnnhub.serveousercontent.com/raw/d/loaderf"
+    }
 }
+
 local function Notify(title, content)
     pcall(function()
         game:GetService("StarterGui"):SetCore("SendNotification", {
@@ -14,14 +20,24 @@ local function Notify(title, content)
         })
     end)
 end
+
 local currentPlaceId = game.PlaceId
 local targetScript = scriptList[currentPlaceId]
+
 if targetScript then
     Notify("Rick HUB", "Loader success")
     task.wait(0.5)
+
     pcall(function()
-        loadstring(game:HttpGet(targetScript))()
+        if type(targetScript) == "string" then
+            loadstring(game:HttpGet(targetScript))()
+        else
+            _G.Hub = targetScript.hub
+            _G.file = targetScript.file
+            loadstring(game:HttpGet(targetScript.url))()
+        end
     end)
+
 else
     Notify("Rick HUB", "MapNotSuppot")
 end
